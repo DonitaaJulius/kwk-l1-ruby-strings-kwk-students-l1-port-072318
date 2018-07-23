2b5d41f8c8325d56da434df580f6266ea00f3f77
DonitaaJulius <donitaajulius@gmail.com>
2018-07-23 22:15:04 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,17 +1,15 @@
-guest_name = gets.strip
-party_name = gets.strip
-date = gets.strip
-time = gets.strip
-
-
 puts "What is the guest's name?"
 puts "My name is #{guest_name}"
+guest_name = gets.strip
 
 puts "What is the party?"
 puts "The party is #{party_name}"
+party_name = gets.strip
 
 puts "When is the party?"
 puts  "The party is on #{date} at #{time}"
+date = gets.strip
+time = gets.strip
 
 puts "Dear" + guest_name 
 puts "You are cordially invited to the " + party_name + " on " + date + " at " + time + "."
